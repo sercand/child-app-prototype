ace806f0ed230a77f87f91984cf4ced462d7b798
Sercan Değirmenci <degirmencisercan@gmail.com>
2015-06-09 10:42:22 +0300

    :fire: working login

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','starter.controllers'])
+angular.module('starter', ['ionic', 'starter.auth', 'starter.controllers'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
@@ -46,6 +46,7 @@ angular.module('starter', ['ionic','starter.controllers'])
             });
 
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/dash');
-
+        var loggedIn = JSON.parse(localStorage.getItem("isLoggedIn"));
+        console.log(loggedIn);
+        $urlRouterProvider.otherwise(loggedIn ? '/dash' : '/login');
     });
--- www/js/auth.js
+++ www/js/auth.js
@@ -1,7 +1,7 @@
 /**
  * Created by Zafer on 9.6.2015.
  */
-angular.module('starter.services.auth', [])
+angular.module('starter.auth', [])
     .factory('$localStorage', function ($window) {
         return {
             set: function (key, value) {
@@ -18,35 +18,48 @@ angular.module('starter.services.auth', [])
             }
         }
     })
-    .factory('Auth', function ($http, $localStorage, DPI) {
-        var authService = {};
+    .factory('Auth', function ($http, $localStorage) {
+        var authService = {
+            get parentId() {
+                return $localStorage.get('parent_id', null);
+            },
+            set parentId(id) {
+                return $localStorage.set('parent_id', id)
+            },
+            get childId() {
+                return $localStorage.get('child_id', null);
+            },
+            set childId(id) {
+                return $localStorage.set('child_id', id);
+            }
+        };
 
-        authService.serverUrl = "http://food.sercand.com";
+        authService.serverUrl = "http://localhost:6001";
         //  authService.serverUrl = "http://10.10.1.81:3000";
         authService.apiUrl = authService.serverUrl + "/api/v1";
 
 
         authService.isLoggedIn = $localStorage.getObject("isLoggedIn") | false;
-        authService.user = $localStorage.getObject("user");
-        authService.userId = $localStorage.getObject("userId");
-        authService.nextResults = [];
-        authService.picture = authService.serverUrl + DPI.picture_path + authService.user._id + "_p.jpg";
 
-        function setUserId(id) {
-            authService.userId = id;
-            $localStorage.setObject("userId", id);
+        authService.parent = $localStorage.getObject("parent");
+        authService.child = $localStorage.getObject("child");
+
+        authService.nextResults = [];
+
+        function setParentData(data) {
+            authService.parent = data;
+            $localStorage.setObject("parent", data);
         }
 
-        function setUserData(data) {
-            authService.user = data;
-            $localStorage.setObject("user", data);
+        function setChildrenData(data) {
+            authService.child = data;
+            $localStorage.setObject("child", data);
             authService.isLoggedIn = true;
             $localStorage.setObject("isLoggedIn", true);
-            authService.picture = authService.serverUrl + DPI.picture_path + authService.user._id + "_p.jpg";
         }
 
-        authService.getProfileData = function (id, done) {
-            $http.get(authService.apiUrl + '/user/profile?id=' + id)
+        authService.getParentData = function (id, done) {
+            $http.get(authService.apiUrl + '/parent/' + id)
                 .success(function (data) {
                     if (data.success) {
                         done(null, data.data);
@@ -58,7 +71,19 @@ angular.module('starter.services.auth', [])
                     return done(data);
                 });
         };
-
+        authService.getChild = function (id, done) {
+            $http.get(authService.apiUrl + '/parent/' + id + '/getchildren')
+                .success(function (data) {
+                    if (data.success && data.data.length > 0) {
+                        done(null, data.data[0]);
+                    } else {
+                        done(data.message);
+                    }
+                })
+                .error(function (data) {
+                    return done(data);
+                });
+        };
         authService.register = function (firstName, secondName, email, password, done) {
             var postData = {
                 first_name: firstName,
@@ -67,16 +92,22 @@ angular.module('starter.services.auth', [])
                 password: password
             };
 
-            $http.post(authService.serverUrl + '/account/register/mail', postData).
+            $http.post(authService.serverUrl + '/account/register', postData).
                 success(function (data, status, headers, config) {
                     console.log("register: " + JSON.stringify(data));
-                    setUserId(data.user_id);
-                    authService.getProfileData(authService.userId, function (err, doc) {
+                    authService.parentId = data.parent_id;
+                    authService.getParentData(data.parent_id, function (err, doc) {
                         if (err) {
                             return done(err);
                         }
-                        setUserData(doc);
-                        return done(null);
+                        setParentData(doc);
+                        authService.getChild(data.parent_id, function (err, docChild) {
+                            if (err) {
+                                return done(err);
+                            }
+                            setChildrenData(docChild);
+                            return done(null);
+                        });
                     });
                 }).
                 error(function (data, status, headers, config) {
@@ -95,17 +126,23 @@ angular.module('starter.services.auth', [])
                 password: password
             };
 
-            $http.post(authService.serverUrl + '/account/login/mail', postData).
+            $http.post(authService.serverUrl + '/account/login', postData).
                 success(function (data, status, headers, config) {
                     console.log("login: " + JSON.stringify(data));
 
-                    setUserId(data.user_id);
-                    authService.getProfileData(authService.userId, function (err, doc) {
+                    authService.parentId = data.parent_id;
+                    authService.getParentData(data.parent_id, function (err, doc) {
                         if (err) {
                             return done(err);
                         }
-                        setUserData(doc);
-                        return done(null);
+                        setParentData(doc);
+                        authService.getChild(data.parent_id, function (err, docChild) {
+                            if (err) {
+                                return done(err);
+                            }
+                            setChildrenData(docChild);
+                            return done(null);
+                        });
                     });
                 }).
                 error(function (data, status, headers, config) {
@@ -117,24 +154,18 @@ angular.module('starter.services.auth', [])
                 });
         };
 
-        authService.searchMenu = function (lat, lng, done) {
-            var path = authService.apiUrl + '/menu/search?lng=' + lng + '&lat=' + lat;
-            $http.get(path)
-                .success(function (body) {
-                    done(null, body.data);
-                })
-                .error(function (data) {
-                    done(data);
-                })
-        };
-
-
         if (authService.isLoggedIn) {
-            authService.getProfileData(authService.userId, function (err, doc) {
+            authService.getParentData(authService.parentId, function (err, doc) {
                 if (err) {
                     return;
                 }
-                return setUserData(doc);
+                setParentData(doc);
+                authService.getChild(doc._id, function (err, docChild) {
+                    if (err) {
+                        return;
+                    }
+                    setChildrenData(docChild);
+                });
             });
         }
 
